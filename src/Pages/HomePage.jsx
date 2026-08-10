3720d662d620bee8d0237a8e7c2cd38716dd1105
Ali Mujahid <mindcracker54321@gmail.com>
2026-08-10 02:36:52 -0400

    Animation for homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,19 +1,84 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
+import gsap from "gsap"
 
 export default function HomePage() {
 
     const [ menuBox, setMenuBox ] = useState(false);
+
+    useEffect(() => {
+
+        var tl = gsap.timeline({
+            repeat: -1
+        })
+
+        tl.to("#content1", {
+            opacity: 1,
+            duration: 0
+        })
+        tl.to("#content1", {
+            opacity: 0,
+            delay: 3,
+            duration: 0
+        })
+        .to("#content2", {
+            opacity: 1,
+            duration: 0
+        })
+        .to("#content2", {
+            opacity: 0,
+            delay: 3,
+            duration: 0
+        })
+        .to("#content3", {
+            opacity: 1,
+            duration: 0
+        })
+        .to("#content3", {
+            opacity: 0,
+            delay: 3,
+            duration: 0
+        })
+
+        return () => tl.kill()
+    }, [])
 
     return (
         <div className="flex flex-col">
             <div className="flex fixed z-40 top-8 left-8">
                 <img className="w-14" src="gusta-logo.avif" alt="Website logo" />
             </div>
-            <div className="flex flex-col py-32 items-center">
+            <div className="flex flex-col py-32 items-center overflow-x-hidden">
                 <h1 className="font-bold px-4 py-2 text-center text-4xl md:text-7xl md:max-w-[700px] max-w-[400px]">Forward Through Digital Design</h1>
-                <div className="bg-slate-400 w-full h-[624px]">
-                    {/* Insert Image here */}
+                <div className="w-full h-[624px] relative">
+                    <div id="content1" className="opacity-0 absolute left-1/2 -translate-x-1/2 w-[550px] h-[624px]">
+                        <video className="w-52 absolute -rotate-3 -left-[3%] top-[300px] rounded-xl" src="https://framerusercontent.com/assets/lLvZTmIKfLwL7G5qVBUXGdhQo.mp4" preload="auto" autoPlay loop muted poster="https://framerusercontent.com/images/dZrjOw82qYCOxUCOashJHT8oaZY.png?width=360&height=640"></video>
+                        <img className="absolute top-[400px] -right-12 rotate-3 w-96 h-64 rounded-3xl" src="https://framerusercontent.com/images/qYVaoPHzFIbni6dJOLvx5SAlYGk.png?scale-down-to=1024&width=960&height=1120 877w,https://framerusercontent.com/images/qYVaoPHzFIbni6dJOLvx5SAlYGk.png?width=960&height=1120 960w" alt="" />
+                        <img className="w-80 rounded-xl rotate-6 absolute top-[30px] -left-3" src="https://framerusercontent.com/images/yq0IGMMzJtKXkVuRq470oUtnM0.jpg?scale-down-to=512&width=1000&height=740 512w,https://framerusercontent.com/images/yq0IGMMzJtKXkVuRq470oUtnM0.jpg?width=1000&height=740 1000w" alt="" />
+                        <img className="w-56 -rotate-3 absolute top-48 -right-4 rounded-3xl" src="https://framerusercontent.com/images/MrtB1IFGvmEkWTjmnuvYcOHSgM.jpg?scale-down-to=512&width=660&height=560 512w,https://framerusercontent.com/images/MrtB1IFGvmEkWTjmnuvYcOHSgM.jpg?width=660&height=560 660w" alt="" />
+                        <img className="w-44 rounded-3xl absolute right-0 rotate-6" src="https://framerusercontent.com/images/LbLTTeXIMS3l2pO0OnyTFePogo.png?scale-down-to=512&width=600&height=600 512w,https://framerusercontent.com/images/LbLTTeXIMS3l2pO0OnyTFePogo.png?width=600&height=600 600w" alt="" />
+                        <img className="w-24 absolute top-[300px] left-56" src="https://framerusercontent.com/images/AwjThmm5Qa0BI05po6eHlv6vHSE.png?width=300&height=234" alt="" />
+                    </div>
+                    <div id="content2" className="opacity-0 absolute left-1/2 -translate-x-1/2 w-[550px] h-[624px]">
+                        <img className="w-44 rounded-full absolute rotate-6 left-8" src="https://framerusercontent.com/images/VxoTEviyPKo09XuXJigjmmaPcsc.jpg?width=640&height=920 640w" alt="" />
+                        <img className="absolute w-72 rotate-6 bottom-4 rounded-full -right-8 bg-yellow-400 px-6 py-4" src="https://framerusercontent.com/images/EMQNMW35h7D8LENGmKJJkkcKg.png?scale-down-to=512&width=800&height=330 512w,https://framerusercontent.com/images/EMQNMW35h7D8LENGmKJJkkcKg.png?width=800&height=330 800w" alt="" />
+                        <video className="w-56 absolute bottom-44 rounded-3xl right-0" autoPlay loop muted src="https://framerusercontent.com/assets/8zqBjfXanNa1XpaIzKT2PW75ak.mp4"></video>
+                        <img className="w-48 absolute left-8 -bottom-6 rounded-3xl rotate-12" src="https://framerusercontent.com/images/KUhX8gQK2Ytrp1MlkqgmwCHhH0.jpg?width=643&height=840" alt="" />
+                        <video className="absolute w-44 rounded-3xl -rotate-6 right-4" loop autoPlay muted src="https://framerusercontent.com/assets/laGrVxebPqdjgUpnNzPB5d4vfE.mp4"></video>
+                        <video className="w-48 absolute rounded-3xl top-[200px] left-32 -rotate-6" loop muted autoPlay src="https://framerusercontent.com/assets/LhSo2NvVJcTAG0F1fRavHkc.mp4"></video>
+                        <img className="w-24 absolute left-48 top-[100px] -rotate-12" src="https://framerusercontent.com/images/3LS14LWMJvsGn6gXywuvtwBqHE.png?width=450&height=612 450w" alt="" />
+                        <img className="w-40 absolute left-40 bottom-6" src="https://framerusercontent.com/images/P70Yh5NQDps6cgriD2sKWhLpI.png?width=781&height=465" alt="" />
+                    </div>
+                    <div id="content3" className="opacity-0 absolute left-1/2 -translate-x-1/2 w-[550px] h-[624px]">
+                        <img className="w-60 h-64 absolute right-8 -rotate-6 rounded-3xl top-56" src="https://framerusercontent.com/images/xPkMQNqVicuf9ASIlzKvbEcUDY.jpg?width=1040&height=920" alt="" />
+                        <img className="w-64 absolute -bottom-12 left-6 rounded-[50px] -rotate-6" src="https://framerusercontent.com/images/EFvgHvy3aSaDGW9vcn7sDmvsg.png?width=1040&height=1040" alt="" />
+                        <img className="w-60 absolute rounded-3xl top-4 -rotate-6" src="https://framerusercontent.com/images/A5F8uo7VL5EcRnNHef1DeRhmPDQ.jpg?width=1000&height=740" alt="" />
+                        <video className="w-48 absolute rounded-3xl rotate-3 left-12 top-[210px]" loop muted autoPlay src="https://framerusercontent.com/assets/j44HvIGeouTMmh0w41xzipuF9o.mp4"></video>
+                        <video className="w-44 absolute right-8 -bottom-12 rounded-3xl rotate-6" loop autoPlay muted src="https://framerusercontent.com/assets/TU7cHjIr2E44lZ20TcFYKqyqF0.mp4"></video>
+                        <video className="w-60 absolute right-14 top-6 -rotate-3 rounded-3xl" loop autoPlay muted src="https://framerusercontent.com/assets/9ly0T7RZY6WBkHqlwQvZBOUnR3w.mp4"></video>
+                        <img className="w-40 absolute top-40 left-40" src="https://framerusercontent.com/images/70EAw0GRWLLWf2k7oi1sth6rVY.png?width=750&height=630" alt="" />
+                        <img className="w-28 rotate-6 absolute bottom-10 left-60" src="https://framerusercontent.com/images/lwjDF4oM1o8BkiG9VVrRLUQzik8.png?width=483&height=420" alt="" />
+                    </div>
                 </div>
                 <div className="flex flex-col py-20 px-10 md:text-center md:px-64 text-4xl md:text-5xl font-normal gap-8 md:gap-14">
                     <p>We are Gusta, a design studio rooted in craft, curosity, and care.</p>
