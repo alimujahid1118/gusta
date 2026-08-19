b139036759a7034b69416475b3a904e081d55972
Ali Mujahid <mindcracker54321@gmail.com>
2026-08-19 08:41:47 -0400

    About page for mobile completed

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -10,6 +10,60 @@ export default function About () {
     const rollerRef = useRef(null)
     const firstSetRef = useRef(null)
 
+    const items = [
+        {
+            id: 1,
+            type: "video",
+            media: "https://framerusercontent.com/assets/GePJDstrr0sQiV2cR8beEyPoQk.mp4",
+            title: "En Temporada",
+            description:
+                "Our initiative to help make a conscious decision on the fruit and vegetables we consume",
+            caseStudy: true,
+        },
+        {
+            id: 2,
+            type: "image",
+            media: "https://framerusercontent.com/images/s5OBi4dBhlSZWoBBoNdGMPNzS0.jpg?width=1310&height=1310",
+            title: "Awwwards Jury",
+            description: "Part of a global panel of specialists in the creative industry that assess the best web projects in the world.",
+        },
+        {
+            id: 3,
+            type: "image",
+            media: "https://framerusercontent.com/images/8pXy4cvIIf8Osf2MRjh2DQIUvM.jpg?width=1200&height=1600",
+            title: "Politechnical University of Valencia",
+            description: "We teach classes about UX/UI design in the Design and Illustration master (MDI) and Interface Design diploma (DDI).",
+        },
+        {
+            id: 4,
+            type: "video",
+            media: "https://framerusercontent.com/assets/ej9n5wAa1vN9uRDTRZj2juXCu3k.mp4",
+            title: "Loop",
+            description: "Being part of globally distributed team to help design and build a humanitarian and development aid platform.",
+        },
+        {
+            id: 5,
+            type: "image",
+            media: "https://framerusercontent.com/images/WxMeBSJZLvNSD9FjWyo51wt2mU.png?width=1310&height=1310",
+            title: "Conference talks and events",
+            description: "We’re grateful to speak and share our vision on digital product design at conferences and events, including our recent talk at VLC Testing.",
+        },
+        {
+            id: 6,
+            type: "image",
+            media: "https://framerusercontent.com/images/JNMBinio7bQaGdWO2813Cztc16g.png?width=1310&height=1310",
+            title: "Part of local professional associations",
+            description: "We’re part of several associations within the Valencian Community, such as ITI and ADCV, to help the industry move forward.",
+        },
+        {
+            id: 7,
+            type: "video",
+            media: "https://framerusercontent.com/assets/iR7FbL7RHD1mUb9YVnM4RD5G2k.mp4",
+            title: "Face This",
+            description: "Designing a new website for a charity streetwear brand to help raise funds for improving education in Indonesia.",
+        },
+    ]
+
     useEffect(() => {
         const roller = rollerRef.current
         const firstSet = firstSetRef.current
@@ -33,15 +87,49 @@ export default function About () {
         }
     }, [])
 
+    useEffect(() => {
+        const tl = gsap.timeline({ repeat: -1 })
+
+        items.forEach((item) => {
+            tl.to(`#item${item.id}`, {
+                opacity: 1,
+                duration: 0,
+            })
+
+            tl.to(`#item${item.id}`, {
+                opacity: 0,
+                duration: 0,
+                delay: 5,
+            })
+        })
+
+        return () => tl.kill()
+    }, [])
+
+    useEffect(() => {
+        const bars = gsap.utils.toArray(".bar")
+
+        const tl = gsap.timeline({ repeat: -1 })
+
+        bars.forEach((bar) => {
+            tl.to(bar, {
+                x: 40,
+                duration: 5,
+            })
+        })
+
+        return () => tl.kill()
+    }, [])
+
     return (
         <div>
             <Logo />
             <MenuBox />
 
             <div className="bg-[#0ba5bd]">
-                <div className="text-white flex flex-col gap-4 px-8 pt-36">
-                    <p>About</p>
-                    <p className="text-3xl font-semibold pr-[140px]">We’re a design studio shaped by the people we design for, and the people we design with.</p>
+                <div className="text-white flex flex-col gap-4 px-8 pt-36 md:pt-44 md:pb-20">
+                    <p className="md:hidden">About</p>
+                    <p className="text-3xl md:text-[40px] md:leading-10 md:max-w-[720px] font-semibold pr-[140px]">We’re a design studio shaped by the people we design for, and the people we design with.</p>
                 </div>
                 <div className="relative text-white pt-10 mb-20">
                     <div className="relative overflow-x-clip h-40">
@@ -51,55 +139,69 @@ export default function About () {
                             <div ref={firstSetRef} className="flex w-max gap-8">
                                 <div className="relative">
                                     <img className="w-64 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/0VX8BhcfCDqj0smnLt93FsJcYWs.jpg?width=720&height=600" alt="" />
+                                    <p className="relative bottom-0 rotate-6 text-sm max-w-64 text-[#505451]">Testing the Software we designed for Jeanologia</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-36 -rotate-6 rounded-[30px]" src="https://framerusercontent.com/images/Vg50OIhhHHwTF72s6jw0erFiHcU.jpg?width=360&height=400" alt="" />
+                                    <p className="relative bottom-0 left-2 -rotate-6 text-sm max-w-40 text-[#505451]">AMS x VLC collaborators</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-40 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/rEHAaHSlxyHZaPlbhyeue1C1bTk.jpg?width=440&height=620" alt="" />
+                                    <p className="relative -top-64 -left-4 -rotate-6 text-sm max-w-40">Design thinking</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-64 -rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/0fX7HCDEI3Qqe0nAl7CkZKzGA.jpg?width=640&height=520" alt="" />
+                                    <p className="relative bottom-0 left-4 -rotate-6 text-sm max-w-64 text-[#505451]">Testing Nixfarma with challenging light conditions</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-64 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/b1pXAWWoLFxO7M6A8kUXlilg.jpg?width=600&height=400" alt="" />
+                                    <p className="relative bottom-0 rotate-6 text-sm max-w-64 text-[#505451]">On-site crop management with Agro4all</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-56 -rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/A2KAOIpF3wHDuETTC4Nsk8n1fE.jpg?width=640&height=520" alt="" />
+                                    <p className="relative -top-[215px] -left-6 text-nowrap -rotate-6 text-sm max-w-56">Making port access efficient at Valenciaport</p>
                                 </div>
                                 <div className="relative">
-                                    <img className="w-36 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/RiWb5QImBvSPXlHQRTuFC9tI4w.jpg?width=320&height=450" alt="" />
+                                    <img className="w-36 relative left-10 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/RiWb5QImBvSPXlHQRTuFC9tI4w.jpg?width=320&height=450" alt="" />
+                                    <p className="relative bottom-0 left-2 rotate-6 text-sm max-w-64 text-[#505451]">UK workshop with Lufthansa</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-56 relative -top-8 -rotate-6 rounded-[120px]" src="https://framerusercontent.com/images/PExJAHh9WQNeCJ4w4qA3vmGqJE.jpg?width=512&height=640" alt="" />
+                                    <p className="relative bottom-12 -right-24 -rotate-12 text-sm max-w-64 text-[#505451]">At the studio</p>
                                 </div>
                             </div>
-
-                            {/* Duplicate set */}
                             <div className="flex w-max gap-8">
                                 <div className="relative">
                                     <img className="w-64 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/0VX8BhcfCDqj0smnLt93FsJcYWs.jpg?width=720&height=600" alt="" />
+                                    <p className="relative bottom-0 rotate-6 text-sm max-w-64 text-[#505451]">Testing the Software we designed for Jeanologia</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-36 -rotate-6 rounded-[30px]" src="https://framerusercontent.com/images/Vg50OIhhHHwTF72s6jw0erFiHcU.jpg?width=360&height=400" alt="" />
+                                    <p className="relative bottom-0 left-2 -rotate-6 text-sm max-w-40 text-[#505451]">AMS x VLC collaborators</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-40 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/rEHAaHSlxyHZaPlbhyeue1C1bTk.jpg?width=440&height=620" alt="" />
+                                    <p className="relative -top-64 -left-4 -rotate-6 text-sm max-w-40">Design thinking</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-64 -rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/0fX7HCDEI3Qqe0nAl7CkZKzGA.jpg?width=640&height=520" alt="" />
+                                    <p className="relative bottom-0 left-4 -rotate-6 text-sm max-w-64 text-[#505451]">Testing Nixfarma with challenging light conditions</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-64 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/b1pXAWWoLFxO7M6A8kUXlilg.jpg?width=600&height=400" alt="" />
+                                    <p className="relative bottom-0 rotate-6 text-sm max-w-64 text-[#505451]">On-site crop management with Agro4all</p>
                                 </div>
                                 <div className="relative">
                                     <img className="w-56 -rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/A2KAOIpF3wHDuETTC4Nsk8n1fE.jpg?width=640&height=520" alt="" />
+                                    <p className="relative -top-[215px] -left-6 text-nowrap -rotate-6 text-sm max-w-56">Making port access efficient at Valenciaport</p>
                                 </div>
                                 <div className="relative">
-                                    <img className="w-36 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/RiWb5QImBvSPXlHQRTuFC9tI4w.jpg?width=320&height=450" alt="" />
+                                    <img className="w-36 relative left-10 rotate-6 rounded-[40px]" src="https://framerusercontent.com/images/RiWb5QImBvSPXlHQRTuFC9tI4w.jpg?width=320&height=450" alt="" />
+                                    <p className="relative bottom-0 left-2 rotate-6 text-sm max-w-64 text-[#505451]">UK workshop with Lufthansa</p>
                                 </div>
                                 <div className="relative">
-                                    <img className="w-56 -rotate-6 rounded-[120px]" src="https://framerusercontent.com/images/PExJAHh9WQNeCJ4w4qA3vmGqJE.jpg?width=512&height=640" alt="" />
+                                    <img className="w-56 relative -top-8 -rotate-6 rounded-[120px]" src="https://framerusercontent.com/images/PExJAHh9WQNeCJ4w4qA3vmGqJE.jpg?width=512&height=640" alt="" />
+                                    <p className="relative bottom-12 -right-24 -rotate-12 text-sm max-w-64 text-[#505451]">At the studio</p>
                                 </div>
                             </div>
                         </div>
@@ -107,9 +209,9 @@ export default function About () {
                     
                 </div>
             </div>
-            <div className="flex flex-col gap-6 pt-28 px-8">
-                <h1 className="text-[32px] font-normal leading-9">We design digital products and brands people love.</h1>
-                <p className="text-[20px] text-[#505451]">We believe in creating more than just functional solutions. We love to craft experiences that bridge people, businesses, and technology by designing products and brands that are intuitive, impactful, and drive progress, all while having fun along the way. We put people first, from the team we nurture and the collaborators we partner with to the people using what we create.</p>
+            <div className="flex flex-col md:max-w-[850px] gap-6 pt-28 px-8 md:pl-60">
+                <h1 className="text-[32px] font-normal leading-9 md:pr-8 md:text-[40px]">We design digital products and brands people love.</h1>
+                <p className="text-[20px] text-[#505451] md:text-[22px]">We believe in creating more than just functional solutions. We love to craft experiences that bridge people, businesses, and technology by designing products and brands that are intuitive, impactful, and drive progress, all while having fun along the way. We put people first, from the team we nurture and the collaborators we partner with to the people using what we create.</p>
             </div>
             <div className="flex flex-col gap-14 py-16 px-8 items-end">
                 <div className="flex relative w-64 h-48">
@@ -228,6 +330,84 @@ export default function About () {
                             <p>Design Director</p>
                         </div>
                     </div>
+                </div>
+            </div>
+            <div className="flex flex-col py-20">
+                <div className="flex flex-col gap-6 pl-8 pr-10">
+                    <h2 className="text-[35px] font-semibold">
+                        Community
+                    </h2>
+                    <p className="text-[#505451] text-[22px]">
+                        Communities are fundamental to great experiences. Whether we’re engaging with people and initiatives around us, connecting with the design community in the virtual space, or annually dedicating some of our time to providing services to charitable organizations—we like to give back.
+                    </p>
+                </div>
+                <div className="flex flex-row gap-2 pt-10 pl-8 pr-10 items-center justify-center">
+                    {items.map((item) => (
+                        <div
+                            key={item.id}
+                            className="relative overflow-x-hidden rounded-2xl"
+                        >
+                            <p className="bar bg-slate-700 absolute -left-10 px-5 py-1 rounded-2xl"></p>
+
+                            <p className="bg-slate-300 px-5 py-1"></p>
+                        </div>
+                    ))}
+                </div>
+                <div className="relative py-10 h-[600px]">
+                    {items.map((item, index) => (
+                        <div
+                            key={item.id}
+                            id={`item${item.id}`}
+                            className={`item absolute w-full px-10 ${
+                                index !== 0 ? "opacity-0" : ""
+                            }`}
+                        >
+                            <div className="flex flex-col rounded-2xl items-center w-full gap-3">
+
+                                <div className="w-full aspect-square max-w-[350px] overflow-hidden rounded-2xl">
+                                    {item.type === "video" ? (
+                                        <video
+                                            className="w-full h-full object-cover"
+                                            src={item.media}
+                                            loop
+                                            autoPlay
+                                            muted
+                                            playsInline
+                                        />
+                                    ) : (
+                                        <img
+                                            className="w-full h-full object-cover"
+                                            src={item.media}
+                                            alt={item.title}
+                                        />
+                                    )}
+                                </div>
+
+                                <div className="flex flex-col gap-3 items-start text-start w-full max-w-[350px]">
+                                    <p className="text-[22px] font-semibold">
+                                        {item.title}
+                                    </p>
+
+                                    <p className="text-[#505451] text-[20px] pr-10">
+                                        {item.description}
+                                    </p>
+
+                                    {item.caseStudy && (
+                                        <Link className="flex flex-row items-center gap-4">
+                                            <p className="text-[18px] font-semibold">
+                                                View Case study
+                                            </p>
+
+                                            <p className="text-[25px] mb-[1px]">
+                                                {`>`}
+                                            </p>
+                                        </Link>
+                                    )}
+                                </div>
+
+                            </div>
+                        </div>
+                    ))}
                 </div>
             </div>
             <div className="bg-[#abe8e8] py-10 flex flex-col items-center justify-center">
